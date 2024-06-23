4ff91677eaf8d9c8521aa077b3cf16b2a05a918d
Ziqi Hua <ziqihua@seas.upenn.edu>
2024-06-23 01:27:45 -0400

    improve execution

--- PlagiarismDetector.java
+++ PlagiarismDetector.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -9,82 +8,79 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
  * This class implements a simple plagiarism detection algorithm.
  */
 public class PlagiarismDetector {
-	
+
 	/*
 	 * Returns a Map (sorted by the value of the Integer, in non-ascending order) indicating
 	 * the number of matches of phrases of size windowSize or greater between each document in the corpus
-	 * 
+	 *
 	 * Note that you may NOT remove this method or change its signature or specification!
 	 */
 	public static Map<String, Integer> detectPlagiarism(String dirName, int windowSize, int threshold) {
 		File dirFile = new File(dirName);
 		String[] files = dirFile.list();
 		if (files == null) throw new IllegalArgumentException();
-		
-		Map<String, Integer> numberOfMatches = new HashMap<String, Integer>();
-		
-		// compare each file to all other files
-		for (int i = 0; i < files.length; i++) {
-			String file1 = files[i];
 
-			for (int j = 0; j < files.length; j++) { 
+		Map<String, Set<String>> filePhrasesMap = new HashMap<>();
+		for (String file : files) {
+			Set<String> phrases = createPhrases(dirName + "/" + file, windowSize);
+			if (phrases != null) {
+				filePhrasesMap.put(file, phrases);
+			}
+		}
+
+		Map<String, Integer> numberOfMatches = new HashMap<>();
+
+		// Compare each file to all other files
+		for (int i = 0; i < files.length; i++) {
+			for (int j = i + 1; j < files.length; j++) {
+				String file1 = files[i];
 				String file2 = files[j];
 
-				// create phrases for each file
-				Set<String> file1Phrases = createPhrases(dirName + "/" + file1, windowSize); 
-				Set<String> file2Phrases = createPhrases(dirName + "/" + file2, windowSize); 
-				
-				if (file1Phrases == null || file2Phrases == null)
-					return null;
-				
-				// find matching phrases in each Set
-				Set<String> matches = findMatches(file1Phrases, file2Phrases);
-				
-				if (matches == null)
-					return null;
+				Set<String> file1Phrases = filePhrasesMap.get(file1);
+				Set<String> file2Phrases = filePhrasesMap.get(file2);
 
-				// if the number of matches exceeds the threshold, add it to the Map
-				if (matches.size() > threshold) {
-					String key = file1 + "-" + file2;
-					if (numberOfMatches.containsKey(file2 + "-" + file1) == false && file1.equals(file2) == false) {
-						numberOfMatches.put(key,matches.size());
+				if (file1Phrases != null && file2Phrases != null) {
+					Set<String> matches = findMatches(file1Phrases, file2Phrases);
+
+					if (matches.size() > threshold) {  // Ensure strictly greater than threshold
+						String key = file1 + "-" + file2;
+						numberOfMatches.put(key, matches.size());
 					}
-				}				
+				}
 			}
-			
-		}		
-		
-		// sort the results based on the number of matches
+		}
+
+		// Sort the results based on the number of matches
 		return sortResults(numberOfMatches);
 	}
-	
-	
+
 	/*
 	 * This method reads the given file and then converts it into a List of Strings.
 	 * It excludes punctuation and converts all words in the file to uppercase.
 	 */
 	private static List<String> readFile(String filename) {
 		if (filename == null) return null;
-		
-		List<String> words = new ArrayList<String>();
-		
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(filename));
+
+		List<String> words = new ArrayList<>();
+		StringBuilder content = new StringBuilder();
+
+		try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
 			String line;
-			while ((line = in.readLine())  != null) {
-				String[] tokens = line.split(" ");
-				for (String token : tokens) { 
-					// this strips punctuation and converts to uppercase
-					words.add(token.replaceAll("[^a-zA-Z]", "").toUpperCase()); 
-				}
+			while ((line = in.readLine()) != null) {
+				content.append(line).append(" ");
 			}
-		}
-		catch (Exception e) {
+			String[] tokens = content.toString().split("\\s+");
+			for (String token : tokens) {
+				// This strips punctuation and converts to uppercase
+				words.add(token.replaceAll("[^a-zA-Z]", "").toUpperCase());
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
@@ -96,89 +92,46 @@ public class PlagiarismDetector {
 	 * each of size "window". The Strings in each phrase are whitespace-separated.
 	 */
 	private static Set<String> createPhrases(String filename, int window) {
-
-		// read the file
 		List<String> words = readFile(filename);
-		
-		if (window < 1) return null;
-		
-		Set<String> phrases = new HashSet<String>();
-		
-		// create phrases of size "window" and add to Set
-		for (int i = 0; i < words.size() - window + 1; i++) {
-			String phrase = "";
+		if (words == null || window < 1) return null;
+
+		Set<String> phrases = new HashSet<>();
+		for (int i = 0; i <= words.size() - window; i++) {
+			StringBuilder phrase = new StringBuilder();
 			for (int j = 0; j < window; j++) {
-				phrase += words.get(i+j) + " ";
+				phrase.append(words.get(i + j)).append(" ");
 			}
-
-			if (phrases.contains(phrase) == false)
-				phrases.add(phrase);
-
+			phrases.add(phrase.toString().trim());
 		}
-		
 		return phrases;
 	}
 
-	
-	
 	/*
 	 * Returns a Set of Strings that occur in both of the Set parameters.
 	 * However, the comparison is case-insensitive.
 	 */
 	private static Set<String> findMatches(Set<String> myPhrases, Set<String> yourPhrases) {
-	
-		Set<String> matches = new HashSet<String>();
-		
-		if (myPhrases != null && yourPhrases != null) {
-		
-			for (String mine : myPhrases) {
-				for (String yours : yourPhrases) {
-					if (mine.equalsIgnoreCase(yours)) {
-						matches.add(mine);
-					}
-				}
+		Set<String> matches = new HashSet<>();
+		Set<String> lowerCaseYourPhrases = yourPhrases.stream()
+				.map(String::toLowerCase)
+				.collect(Collectors.toSet());
+		for (String phrase : myPhrases) {
+			if (lowerCaseYourPhrases.contains(phrase.toLowerCase())) {
+				matches.add(phrase);
 			}
 		}
-		
 		return matches;
 	}
-	
-	
+
 	/*
 	 * Returns a LinkedHashMap in which the elements of the Map parameter
 	 * are sorted according to the value of the Integer, in non-ascending order.
 	 */
 	private static LinkedHashMap<String, Integer> sortResults(Map<String, Integer> possibleMatches) {
-		
-		// Because this approach modifies the Map as a side effect of printing 
-		// the results, it is necessary to make a copy of the original Map
-		Map<String, Integer> copy = new HashMap<String, Integer>();
-
-		for (String key : possibleMatches.keySet()) {
-			copy.put(key, possibleMatches.get(key));
-		}	
-		
-		LinkedHashMap<String, Integer> list = new LinkedHashMap<String, Integer>();
-
-		for (int i = 0; i < copy.size(); i++) {
-			int maxValue = 0;
-			String maxKey = null;
-			for (String key : copy.keySet()) {
-				if (copy.get(key) > maxValue) {
-					maxValue = copy.get(key);
-					maxKey = key;
-				}
-			}
-			
-			list.put(maxKey, maxValue);
-			
-			if (copy.containsKey(maxKey)) {
-				copy.put(maxKey, -1);
-			}
-		}
-		
-		return list;
+		return possibleMatches.entrySet()
+				.stream()
+				.sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+						(e1, e2) -> e1, LinkedHashMap::new));
 	}
-	
-
 }
